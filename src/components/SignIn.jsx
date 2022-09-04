e869b67dca3e96b6f329a774fc364c282befd265
Almusawee <112513161+Almusawee@users.noreply.github.com>
2022-09-04 22:02:14 +0430

    1 file modified

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,9 @@
 function SignIn(){
-  return <div className='w-3/5'>
-    <div className='text-left font-bold'>Company Name</div>
+  return <div className='w-3/5 p-5'>
+    <div className='text-left font-bold'><span className='text-green-500'>Company</span> Name</div>
+    <div className='py-10'>
+      <h2 className='text-3xl font-bold text-green-500 underline underline-offset-8'>Sign in to your account</h2>
+    </div>
   </div>;
 }
 
