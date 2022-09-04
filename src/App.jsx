4d0267c00eb14727bb01bc932cf2f2dd9fa05921
Almusawee <112513161+Almusawee@users.noreply.github.com>
2022-09-04 18:09:21 +0430

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      brand: "Ford",
-      model: "Mustang",
-      color: "red",
-      year: 1964
-    };
-  }
-  changeColor = () => {
-    this.setState({color: "blue"});
-  }
-  render() {
+function App() {
     return (
       <div>
        <h1>Hello World!</h1>
