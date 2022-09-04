3bc6c22d265e3dfab51577ddbe941f14ee9341bc
Almusawee <112513161+Almusawee@users.noreply.github.com>
2022-09-05 02:32:09 +0430

    Update SignIn.jsx

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,7 +5,8 @@ import {MdLockOutline} from 'react-icons/md'
 
 
 function SignIn(){
-  return (<div className='w-3/5 p-5'>
+  return (
+    <div className='w-3/5 p-5'>
     <div className='text-left font-bold'><span className='text-green-500'>Company</span> Name</div>
     <div className='py-10'>
       <h2 className='text-2xl font-bold text-green-500 underline underline-offset-8'>Sign in to your account</h2>
