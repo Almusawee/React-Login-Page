4e4766db8fb0287388578e6261d3e16d60c569a9
Almusawee <112513161+Almusawee@users.noreply.github.com>
2022-09-04 21:41:52 +0430

    1 file modified

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,9 @@
 function SignUp(){
-  return <div className='w-2/5'>
-    <p>Sign Up Section</p>
+  return <div className='w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12'>
+    <h2 className= 'text-3xl font-bold mb-2 underline underline-offset-8'>Hello, Friend!</h2>
+    <p className='mb-2'>Fill up personal info and start journey with us!</p>
+    <a href="#" className='border-2 border-white rounded-full px-12 py-1 inline-block font-semibold hover:bg-white hover:text-green-500'>Sign Up</a>
+    <></>
   </div>;
 }
 
